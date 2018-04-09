cac4260a319524fff815e7a498919af64784d178
Flow Jiang <flowjzh@gmail.com>
2018-04-09 17:35:25 +0800

    Define protocols

--- core/book.py
+++ core/book.py
@@ -1,5 +1,16 @@
 import piecash
 
 book = piecash.create_book(
-    uri_conn="mysql+pymysql://cohirer:Letsgo!@localhost/cohirer_dev?charset=utf8&use_unicode=0", overwrite=True)
+    keep_foreign_keys=True,
+    uri_conn="mysql+pymysql://cohirer:Letsgo!@localhost/pieledger?charset=utf8&use_unicode=0",
+    overwrite=True
+)
+
+acc = piecash.Account(
+    name="My account",
+    type="ASSET",
+    parent=book.root_account,
+    commodity=book.commodities.get(mnemonic="EUR"),
+    placeholder=True)
+
 book.save()
