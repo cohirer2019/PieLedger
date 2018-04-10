6620cb182d16f495644ca1ca67516a3db4cd86ab
Flow Jiang <flowjzh@gmail.com>
2018-04-10 23:05:09 +0800

    Change overwrite to flag for genesis

--- cli/initialize.py
+++ cli/initialize.py
@@ -7,7 +7,7 @@ from core.book import create_book
 
 @click.command()
 @click.option(
-    '--overwrite', type=bool, default=False, help='Overwrite existing data')
+    '--overwrite', is_flag=True, help='Overwrite existing data')
 def genesis(overwrite):
     try:
         create_book(overwrite=overwrite)
