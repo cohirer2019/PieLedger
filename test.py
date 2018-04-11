5fa1d9656a993f18c9d15c7027cb19e6d66ba1f3
Flow Jiang <flowjzh@gmail.com>
2018-04-11 10:36:06 +0800

    Handle extract args for unitest more elegantly

--- test.py
+++ test.py
@@ -11,9 +11,14 @@ if __name__ == '__main__':
     # Add additional arguments
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('--sqlite', '-s', default=False, action='store_true')
-    main.USAGE = main.USAGE.replace(
-        'Options:', 'Options:\n  -s, --sqlite     Use sqlite memory db')
-
     init_args, test_args = parser.parse_known_args()
-    init_test(**vars(init_args))
-    main(verbosity=2, argv=[sys.argv[0]] + test_args)
+
+    class MyUnitTest(main):
+        USAGE = main.USAGE.replace(
+            'Options:', 'Options:\n  -s, --sqlite     Use sqlite memory db')
+
+        def runTests(self):
+            init_test(**vars(init_args))
+            super(MyUnitTest, self).runTests()
+
+    MyUnitTest(verbosity=2, argv=[sys.argv[0]] + test_args)
