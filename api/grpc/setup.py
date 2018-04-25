62bf8a4fc8eb3584362d7d58b3882e0b771660d8
Flow Jiang <flowjzh@gmail.com>
2018-04-25 14:41:22 +0800

    Distribute grpc stub files

---
+++ api/grpc/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='PieLedger-Grpc',
+    version='1.0.1',
+    namespace_packages=['pieledger', 'pieledger.api', 'pieledger.api.grpc'],
+    py_modules=[
+        'pieledger.api.grpc.ledger_pb2',
+        'pieledger.api.grpc.services_pb2',
+        'pieledger.api.grpc.services_pb2_grpc'
+    ],
+    package_dir={'pieledger.api.grpc': ''},
+    install_requires=['protobuf >= 3.5.2'],
+    url='http://dev.smart4e.com/cohirer/pieledger',
+    license='Apache License 2.0',
+    author='CoHirer Inc.',
+    author_email='info@cohirer.com',
+    description='Grpc sub files for PieLedger'
+)
