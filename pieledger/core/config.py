787a3947e55b849473b39e80d6bd5208bd1898e0
Flow Jiang <flowjzh@gmail.com>
2018-05-17 16:40:39 +0800

    Read config from environment

--- pieledger/core/config.py
+++ pieledger/core/config.py
@@ -4,9 +4,13 @@ import sys
 
 import yaml
 
-try:
+
+config_path = os.environ.get('PIELEDGER_CONFIG')
+if not config_path:
     config_path = os.path.abspath(
         os.path.join(os.path.dirname(__file__), '../config.yml'))
+
+try:
     with open(config_path, 'r+') as ymlfile:
         ledger_config = yaml.load(ymlfile)
 except IOError:
